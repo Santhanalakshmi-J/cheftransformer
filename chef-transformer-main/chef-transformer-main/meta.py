2eeb1924bfdd334c41ed0e51fd849bdc57705b94
Santhanalakshmi-J <131514423+Santhanalakshmi-J@users.noreply.github.com>
2023-04-24 21:49:28 +0530

    Add files via upload

---
+++ chef-transformer-main/chef-transformer-main/meta.py
@@ -0,0 +1,45 @@
+HEADER_INFO = """""".strip()
+SIDEBAR_INFO = """
+<div class="contributors font-body text-bold">
+
+
+</p>
+</div>
+"""
+CHEF_INFO = """
+<h2 class="font-title">Welcome to our lovely restaurant! </h2>
+""".strip()
+PROMPT_BOX = "Add custom ingredients here (separated by `,`): "
+STORY = """
+<div class="story-box font-body">
+<p>
+
+</p>
+
+
+
+<p>
+  <em>In the cookbooks (a.k.a <a href="https://huggingface.co/datasets/recipe_nlg">dataset</a>), the food items were referred to as NER. </em>
+</p>
+
+
+</div>
+""".strip()
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """.strip()
+hide_decoration_bar_style = """
+    <style>
+        header {visibility: hidden;}
+    </style>
+""".strip()
+hide_expander = """
+  <style>
+    .css-e370rw e19lei0e1{
+      visibility: hidden;
+    }
+  </style>
+"""
